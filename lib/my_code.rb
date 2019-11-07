5ed2f2878a969101b04806cc1147782cf7cc6532
yash patel <yash82099@yahoo.com>
2019-11-07 16:45:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# My Code here....
+def map_to_negativize(source_array)
+  arr = []
+  x = 0 
+  
+  
+  while x < source_array.length do 
+    arr.push(source_array[x] * -1) 
+    x += 1
+  end
+  return arr
+  
+  
+end
+
+def map_to_no_change(source_array)
+  arr = []
+  x = 0
+  while x < source_array.length do 
+    arr.push(source_array[x])
+    x += 1 
+  end 
+  return arr
+end 
+
+
+
+
+
+
